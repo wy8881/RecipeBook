e939b4921e2d41bd77a90a4928d2fcae03206c4b
wy8881 <88151174+wy8881@users.noreply.github.com>
2024-03-20 23:47:40 +1030

    Add router

---
+++ src/app/app-rounting.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {RecipeComponent} from "./recipe/recipe.component";
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+const appRoutes: Routes = [
+  {path:'', redirectTo: '/recipes', pathMatch: 'full'},
+  {path:'recipes', component: RecipeComponent},
+  {path:'shopping-list', component: ShoppingListComponent}
+]
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRountingModule {
+
+}
