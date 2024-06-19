a85253f26ea320f5142caaad993270e967fec9c5
wy8881 <88151174+wy8881@users.noreply.github.com>
2024-06-19 21:25:33 +0930

    Auto-generated commit

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-SRD5BX7K.mjs";import"./chunk-NU5RZGHQ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/shared/ingredient.model.ts
+++
@@ -1,6 +0,0 @@
-export class Ingredient {
-  constructor(public name:string, public amount:number){
-    this.name = name;
-    this.amount = amount;
-  }
-}
